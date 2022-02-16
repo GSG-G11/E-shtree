5f25e87f4ad6d2a3cfd798b4c0f084700843cc1e
unknown <stm1998@hotmail.com>
2022-02-16 19:06:11 +0200

    saleha

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const router = require('./router');
+require('env2')('.env');
 
 const server = http.createServer(router);
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port);
