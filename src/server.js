0fb16f9c55ad65496a5d9a9106f57a79665c216b
unknown <stm1998@hotmail.com>
2022-02-16 19:01:33 +0200

    sdf

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const router = require('./router');
-
+const env = require('env2')('./path-to-your/.env');
 const server = http.createServer(router);
-const port = 5000;
+const port =p 5000;
 server.listen(port);
