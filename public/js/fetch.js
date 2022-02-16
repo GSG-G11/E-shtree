1f2b51017a0c40f36c18c28a359c279928027761
unknown <stm1998@hotmail.com>
2022-02-16 06:28:35 +0200

    create auto complete functionality relates #12

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,10 +1,47 @@
-(function fetch(url) {
+var input = document.querySelector(".search-input");
+var ul = document.getElementById("search-result");
+const debounce = (func, delay) => {
+    let debounceTimer
+    return function () {
+        const context = this
+        const args = arguments
+        clearTimeout(debounceTimer)
+        debounceTimer
+            = setTimeout(() => func.apply(context, args), delay)
+    }
+}
+input.addEventListener('keyup', debounce(function () {
+    fetch('/products', input.value);
+}, 1500));
+
+function fetch(url, userData) {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 let data = JSON.parse(xhr.responseText);
-                console.log(data)
+                let arr = data.filter((el) => {
+                    if (userData) {
+                        return el.title.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+                    }
+                });
+                ul.innerHTML = '';
+                if (arr.length > 0) {
+                    arr.forEach(element => {
+                        const li = document.createElement('li');
+                        li.innerText = element.title;
+                        ul.appendChild(li);
+                    });
+                }else{
+                    const li = document.createElement('li');
+                    li.innerText = 'no results';
+                    ul.appendChild(li);  
+                }
+                // let lis  = arr.map((li)=>{
+                //     return li = `<li>${li}</li>`;
+                // });
+
+                console.log(arr)
             } else if (xhr.status === 404) {
                 faild(timeZoon, cityWeather)
             }
@@ -12,4 +49,4 @@
     }
     xhr.open("GET", url);
     xhr.send();
-})('/products');
+}
